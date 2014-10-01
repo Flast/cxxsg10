0dcfb0db7ebd9aba20533d96e7836f3d3577fb87
Kohei Takahashi <flast@flast.jp>
2014-10-02 02:23:28 +0900

    first commit
    Signed-off-by: Kohei Takahashi <flast@flast.jp>

---
+++ cxxsg10/cxxsg10.hpp
@@ -0,0 +1,17 @@
+//          Copyright Kohei Takahashi 2014.
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef CXXSG10_cxxsg10_hpp_
+#define CXXSG10_cxxsg10_hpp_
+
+#include <cxxsg10/detail/config.hpp>
+
+#include <cxxsg10/detail/compiler.hpp>
+
+#include <cxxsg10/detail/stdlib.hpp>
+
+#endif // CXXSG10_cxxsg10_hpp_
+
---
+++ cxxsg10/detail/config.hpp
@@ -0,0 +1,13 @@
+//          Copyright Kohei Takahashi 2014.
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef CXXSG10_detail_config_hpp_
+#define CXXSG10_detail_config_hpp_
+
+#define CXXSG10_VERSION(major, minor, patch) ((((major * 10000) + minor) * 10000) + patch)
+
+#endif // CXXSG10_detail_config_hpp_
+
